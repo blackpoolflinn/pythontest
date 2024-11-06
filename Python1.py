805babb6af011352d7249fe1e31f4994e9241108
blackpoolflinn <30250526@blackpool.ac.uk>
2024-11-06 10:11:01 +0000

    Update Python1.py

--- Python1.py
+++ Python1.py
@@ -1,13 +1,16 @@
 # Python program to calculate the area of a rectangle
+done = False
 
 def calculate_area(length, width):
     area = length + width
     return area
 
-length = input("Enter the length of the rectangle: ")
-width = input("Enter the width of the rectangle: ")
-
-length = int(length)
-width = int(width)
+while not done:
+    try:
+        length = int(input("Enter the length of the rectangle: "))
+        width = int(input("Enter the width of the rectangle: "))
+        done = True
+    except:
+        print('Please enter an integer')
 
 print("The area of the rectangle is:", calculate_area(length, width))
